b6eab116df43250d35ac71e46ea6f6caa2f965df
Raoul Madaliou BOUYO <raoul.bouyo@transvie.sn>
2025-04-15 13:09:07 +0000

    Retrait de frontend/.next du suivi Git

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MDrYH8zUfAVQlm2wGRjvTUHV+ynGcDykYpZ7cyPHSdE=\"\n}"
--- frontend/.next/static/chunks/src_app_login_page_tsx_8450df25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_0b926897._.js"
-],
-    source: "dynamic"
-});
